2ccee5b806c334c840bd9645e20f2425c47ed186
ggonu20 <g.gonzalezn@utem.cl>
2022-12-14 16:09:57 -0300

    Funcionalidades basicas

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,1 +1,22 @@
-console.log('pop up');
+console.log("popup!")
+
+//Funcion boton iniciar sesion
+function i_sesion(){
+    alert('iniciar sesion');
+}
+
+//Funcion boton buscar producto
+function b_producto(){
+    alert('buscar producto');
+}
+
+//Funcion boton opciones
+function p_opciones(){
+    //alert('Abriendo pagina opciones');
+    chrome.runtime.openOptionsPage();
+}
+
+//Botones
+document.getElementById('btn_1').onclick = i_sesion; 
+document.getElementById('btn_2').onclick = b_producto; 
+document.getElementById('btn_3').onclick = p_opciones; 
